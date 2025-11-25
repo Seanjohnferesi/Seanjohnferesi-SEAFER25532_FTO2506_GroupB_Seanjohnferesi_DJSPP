0274d3b287a62323ddce50056ad113d00e1f89db
Seanjohnferesi <seanjohnferesi27@gmail.com>
2025-11-25 16:55:22 +0200

    Feat:added  smooth scrolling to carousel

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -1,31 +1,58 @@
-import { usePodcast } from "../context/PodcastContext"
+import { usePodcast } from "../context/PodcastContext.jsx"
 import "../styles/carousel.css"
+import { genres } from "../data.js";
+import { getGenreTitle } from "../utils/getGenreTitle.js"
+import { useRef } from "react";
 
-export default function Carousel() {
+export default function Carousel({}) {
     const {podcasts} = usePodcast()
+    const carouselRef = useRef(null)
+
+    const scrollLeft = () => {
+        carouselRef.current.scrollBy({ 
+            left: -270,
+            behaviour: "smooth"})
+    }
+
+    const scrollRight = () => {
+        carouselRef.current.scrollBy({ 
+            left: 270,
+            behaviour: "smooth"})
+    }
 
     return (
         <section className="Carousel">
             <h2>Recommended Shows</h2>
             <div className="carousel-container">
-                <button>&#10146;</button>
-                <div className="carousel-content">
-                    {podcasts.map(podcast => 
-                    <div className="carousel-card">
-                                <div className="carousel-img-div">
-                                    <img src={podcast.image} className="carousel-pod-img" alt={podcasts.title} />
-                                </div>
-                    
-                                <h2 className="pod-title">Little Nightmares</h2>
-                                        
-                                <div className="genre-wrapper">
-                                        <div className="genre-item">
-                                            history
-                                        </div>
-                                </div>
-                    </div>).slice(3, 10)}
+                <button 
+                    className="leftBtn"
+                    onClick={scrollLeft}
+                >&#10146;</button>
+                <div className="carousel-content" ref={carouselRef}>
+                    {podcasts.slice(3, 14).map(podcast => {
+                        const showGenres = getGenreTitle(podcast.id, genres)
+
+                        return (
+                            <div className="carousel-card" key={podcast.id}>
+                                    <div className="carousel-img-div">
+                                        <img src={podcast.image} className="carousel-pod-img" alt={podcasts.title} />
+                                    </div>
+                        
+                                    <h2 className="pod-title">{podcast.title}</h2>
+                                {showGenres.map((gen, index) => (   
+                                    <div className="genre-wrapper" key={index}>
+                                            <div className="genre-item" >
+                                                {gen}
+                                            </div>
+                                    </div>))}
+                            </div>
+                        )
+                    })}
                 </div>
-                <button>&#10146;</button>
+                <button 
+                    className="rightBtn"
+                    onClick={scrollRight}
+                >&#10146;</button>
             </div>
         </section>
     )
